8e24b84fffc53514591f13bf6fa81b26ed5192c5
Dhanashree Lale <dhanashreelale@Dhanashrees-MacBook-Pro.local>
2024-05-10 14:22:31 -0500

    done with greedy 2

---
+++ queueReconstruction.java
@@ -0,0 +1,23 @@
+// Time Complexity : O(n^2)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a,b) ->{
+            if(a[0] == b[0]){
+                return a[1] - b[1];
+            }
+            return b[0] - a[0];
+        });
+        List<int[]> li = new ArrayList<>();
+        for(int[] person : people){
+            li.add(person[1], person);
+        }
+        int[][] result = new int[people.length][2];
+        for(int i = 0; i < li.size(); i++){
+            result[i] = li.get(i);
+        }
+        return result;       
+    }
+}
